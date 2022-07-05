6d3ec17a28e522ac7daad791ca1179988cbfae5e
amirfzd <amiralifm1407@gmail.com>
2022-07-06 02:39:06 +0430

    practiced redirect dynamic response

--- URLs&view sec/monthly_challenges/challenges_app/urls.py
+++ URLs&view sec/monthly_challenges/challenges_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('<op>' , views.allops)    
+path('<int:op>' , views.allops_numbers),
+path('<str:op>' , views.allops),
 ]
--- URLs&view sec/monthly_challenges/challenges_app/views.py
+++ URLs&view sec/monthly_challenges/challenges_app/views.py
@@ -1,11 +1,20 @@
 import re
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse , HttpResponseNotFound
+from django.http import HttpResponse , HttpResponseNotFound , HttpResponseRedirect
 # Create your views here.
+views_dict ={
+    'op1': 'op1 called !!',
+    'op2': 'op2 called !!',
+    'op3': 'op3 called !!',
+    'op4': 'op4 called !!'
+}
+def allops_numbers(request , op):
+    response_ls= list(views_dict.keys())
+    return HttpResponseRedirect('/challenges_app/'+response_ls[op])
 
 def allops (request , op): 
-    if op =='op1'or op =='op2' or op =='op3':
-        return HttpResponse(f'{op} was called!!!')
+    if op in views_dict.keys():
+        return HttpResponse(f'{views_dict[op]}  was called!!!')
     else:
         return HttpResponseNotFound(f'{op} was not found in urls!!!')
