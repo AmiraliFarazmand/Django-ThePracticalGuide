5db73a8ce80bf4cad12f74c11e60235791ac82e5
amirfzd <amiralifarazmand80@gmail.com>
2022-07-24 17:23:52 +0330

    main is empty

--- 1URLs&view sec/monthly_challenges/challenges_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-path('', views.index_func),
-path('<int:op>' , views.allops_numbers ,name='reverse_challange'),
-path('<str:op>' , views.allops ,name = 'reverse_challange'),
-]
