5c8c3a5ecf65c1aa342905604c27c803f66050c0
amirfzd <amiralifarazmand80@gmail.com>
2022-07-30 15:02:17 +0330

    worked with global static files

--- codingAlong/codingAlong/codingAlong/settings.py
+++ codingAlong/codingAlong/codingAlong/settings.py
@@ -58,7 +58,6 @@ TEMPLATES = [
         'DIRS': [
             # BASE_DIR / 'challenges_app' / 'templates', 
             BASE_DIR / 'templates' ,
-            BASE_DIR
             ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,9 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS= [
+    BASE_DIR / 'static',
+    # "../statics/"
+    # 'd/code ex/Django-ex/django-thePracticalGuide/codingAlong/statics/styles.css',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
