172bcf537a9389434d496b3cc9be92f411f3d099
amirfzd <amiralifarazmand80@gmail.com>
2022-08-13 15:30:32 +0330

    worked with slugfield and overwriting save function. worked with slugify()

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,5 +1,7 @@
+from urllib import request
 from django.db import models
-
+from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
 
 class Book(models.Model):
@@ -7,6 +9,15 @@ class Book(models.Model):
     rate = models.IntegerField(default=0)
     author = models.TextField(null=True , blank=True)
     top_selling  = models.BooleanField(default=False )
-    
+    slug = models.SlugField(default="" , null=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        
+    def get_detailed(self):
+        return reverse("book_detail", args=[self.slug])
+        
     def __str__(self) -> str:
         return f"title:{self.title} , rate:{self.rate} "
+    
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from .models import Book
+from django.http import Http404
 # Create your views here.
 
 
 def index(request):
     all_books = Book.objects.all()
     return render(request, "book_outlet/index.html" , { "books": all_books })
+
+def book_detail(request , slug ):
+    try:
+        book = Book.objects.get(slug=slug)
+        return render(request, "book_outlet/book_detail.html" , { 
+                                                    "title": book.title, 
+                                                    "book": book.title ,  
+                                                    "author" : book.author , 
+                                                    "rate" : book.rate, 
+                                                    "top_selling": book.top_selling ,
+                                                    })
+    except :
+        raise Http404
