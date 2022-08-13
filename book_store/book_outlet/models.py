d9d3cda50a9f2a591ebc16f9c3c21af6de8f7769
amirfzd <amiralifarazmand80@gmail.com>
2022-08-13 12:22:06 +0330

    updated models file and inserted some rows

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     rate = models.IntegerField(default=0)
+    author = models.TextField(null=True , blank=True)
+    top_selling  = models.BooleanField(default=False )
+    
+    def __str__(self) -> str:
+        return f"title:{self.title} , rate:{self.rate} "
