be205e0c56dd2793e68a12f4169f86f364b4e586
amirfzd <amiralifarazmand80@gmail.com>
2022-08-15 13:47:45 +0330

    created 3 models as said(by myself;havn't followed tutorial yet to edit them).

--- project/my_site/blog/models.py
+++ project/my_site/blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model): 
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    
+    def __str__(self) -> str:
+        return f"{self.first_name}-{self.last_name}[{self.id}]"
+# --------------------------------------------------------------------------------------
+class Tag(models.Model):
+    captions = models.TextField(max_length=255)
+    
+    def __str__(self) -> str:
+        return f"[{self.id}](C:{self.captions})"
+# --------------------------------------------------------------------------------------
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.TextField(max_length=200)
+    content = models.TextField(max_length=1000)
+    image_name = models.CharField(max_length=50)
+    date= models.DateField(auto_now=True)
+    slug = models.CharField(max_length=50)  
+    author = models.ForeignKey(Author ,on_delete=models.SET_NULL ,null=True )
+    tag = models.ManyToManyField(Tag)
+    
+    def __str__(self) -> str:
+        return f"[{self.id}]-T:{self.title}-A:{self.author}"
