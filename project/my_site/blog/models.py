12afe677588ccacbb1ace377ca294f72ea79c5b4
amirfzd <amiralifarazmand80@gmail.com>
2022-08-17 13:32:41 +0330

    edited sth based on videos.

--- project/my_site/blog/models.py
+++ project/my_site/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from django.core.validators import MinLengthValidator
 # Create your models here.
 
 class Author(models.Model): 
@@ -11,7 +12,7 @@ class Author(models.Model):
         return f"{self.first_name}-{self.last_name}[{self.id}]"
 # --------------------------------------------------------------------------------------
 class Tag(models.Model):
-    captions = models.TextField(max_length=255)
+    captions = models.CharField(max_length=20)
     
     def __str__(self) -> str:
         return f"[{self.id}](C:{self.captions})"
@@ -19,11 +20,12 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.TextField(max_length=200)
-    content = models.TextField(max_length=1000)
+    content = models.TextField(max_length=5000 ,validators=[MinLengthValidator(limit_value=10)])
     image_name = models.CharField(max_length=50)
     date= models.DateField(auto_now=True)
-    slug = models.CharField(max_length=50)  
-    author = models.ForeignKey(Author ,on_delete=models.SET_NULL ,null=True )
+    # slug = models.CharField(max_length=50)
+    slug = models.SlugField(db_index=True , unique=True )  
+    author = models.ForeignKey(Author ,on_delete=models.SET_NULL ,null=True  ,related_name="posts")
     tag = models.ManyToManyField(Tag)
     
     def __str__(self) -> str:
