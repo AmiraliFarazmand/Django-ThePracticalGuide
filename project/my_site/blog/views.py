4ef992c9b9b4d36486aeda8df1074894bdd8df68
amirfzd <amiralifarazmand80@gmail.com>
2022-08-17 15:19:26 +0330

    END of the section . handled all pages by database instead of hardcoding data.

--- project/my_site/blog/views.py
+++ project/my_site/blog/views.py
@@ -1,66 +1,10 @@
 from django.http import Http404, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 from  datetime import date
 from django.template.loader import render_to_string
+from .models import Post
 all_posts = [
-   {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Maximilian",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-    
+        
 ]
 # Create your views here.
 def get_date(post):
@@ -68,19 +12,27 @@ def get_date(post):
 
 '''Responde functions : ============================================================'''
 def start_page(request):
-    sorted_posts =sorted(all_posts , key = get_date)
-    latest_posts = sorted_posts[-3:]
+    # sorted_posts =sorted(all_posts , key = get_date)
+    # latest_posts = sorted_posts[-3:]
     # return render(request , './templates/blog/index.html')
+    latest_posts = Post.objects.all().order_by("date")[:3]
     return render(request , 'blog/index.html' ,{
         "posts" : latest_posts})
     # return ('salam')
 
 def posts_page(request):    
+    all_posts = Post.objects.all()
     return render(request , 'blog/all-posts.html' , {"all_posts":all_posts})
 
 def post_detail(request , slug):
-    specific_post = next(post for post in all_posts if slug ==post["slug"])
-    return render(request , 'blog/post_detail.html' ,{'post':specific_post})
+    # specific_post = next(post for post in all_posts if slug ==post["slug"])
+    # specific_post = Post.objects.get(slug=slug) 
+    specific_post = get_object_or_404(Post , slug=slug)
+    return render(request , 'blog/post_detail.html' ,{
+        'post':specific_post ,
+        "post_tags":specific_post.tag.all()
+        })
+
 def not_found(request , string):
     response  = render_to_string( '404.html')
     # raise Http404
