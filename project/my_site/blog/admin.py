b6054916903c0c45769eb1da8b30c4d03a37f2b6
amirfzd <amiralifarazmand80@gmail.com>
2022-08-17 14:05:46 +0330

    added sth to admin options + added some posts.

--- project/my_site/blog/admin.py
+++ project/my_site/blog/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Post , Author, Tag
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Author)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title","author" , "date")
+    list_filter= ("author" , "date" ,"tag")
+    prepopulated_fields  ={"slug" : ("title" ,) ,}
+class AuthorAdmin(admin.ModelAdmin):
+    list_display =("id" ,"first_name","last_name" ,"email" ,)
+admin.site.register(Post , PostAdmin)   
+admin.site.register(Author ,AuthorAdmin)
 admin.site.register(Tag)
