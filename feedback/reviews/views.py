75c91cd53a6c012e9ee000e16ca20c69b16ea250
amirfzd <amiralifarazmand80@gmail.com>
2022-08-20 13:36:03 +0330

    get familiar with csrf and post method.

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,7 +1,15 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review (request):
-    return render(request ,template_name='review.html')
+    if request.method == "POST":
+        users_input = request.POST["username"]
+        print(users_input , "***")
+        return HttpResponseRedirect("/thank-you")
+    
+    return render(request ,template_name='reviews/review.html')
+
+def thank_you (request ):    
+    return render(request , "reviews/thank_you.html" )
