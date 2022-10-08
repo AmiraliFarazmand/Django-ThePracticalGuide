c4057ed40b7b71292b44d2515721d5effb728526
amirfzd <amiralifarazmand80@gmail.com>
2022-10-08 13:42:12 +0330

    got familiar with fileuploads. worked with forms on file uploading.

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
+from .forms import ProfileForm
 # Create your views here.
 
-
+def store(file):
+    with open("temp/img.jpg" ,"wb") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+    
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {"form" : form})
 
     def post(self, request):
-        pass
+        # img = request.FILES["image"]
+        # print(img , "***")
+        # store(img)
+        submitted_form  =ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            store(request.FILES["profile_image"])
+            return HttpResponseRedirect("/profiles")        
+        else:
+            return render(request, "profiles/create_profile.html" , {"form":submitted_form})
