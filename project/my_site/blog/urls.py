6f8b4c9659f4803b5bb36d79469d192c5dc1a9c5
amirfzd <amiralifarazmand80@gmail.com>
2022-11-09 14:43:28 +0330

    worked with file uploads(using a Post.image attribute which is from  ImageField, instead of using Post.image_name attribute.)

--- project/my_site/blog/urls.py
+++ project/my_site/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("" , views.start_page , name="index"), 
     path("posts" , views.posts_page , name="all_posts"),
     path("posts/<slug:slug>" , views.post_detail , name="post_detail_page"),
     path("<str:string>" , views.not_found , name="not_found")
-]
+]+ static( settings.MEDIA_URL , document_root  =settings.MEDIA_ROOT)
